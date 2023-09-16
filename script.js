f3d244d50fb4ecdf7037e68603de113ddf6a7be4
Dorir <rodrigo.duarte@upa.edu.py>
2023-09-16 15:00:15 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -3,70 +3,56 @@ const CALCULAR = document.getElementById('calcular');
 const ERROR = document.getElementById('error');
 const FLU = document.getElementById('flu');
 const MAN = document.getElementById('man');
-const MET = document.getElementById('metodo');
-const HOL = document.getElementById('Holliday');
-const VC = document.getElementById('vc');
-const TIT = document.getElementById('titulo');
+
+
 
 CALCULAR.addEventListener('click', () => {
     console.log('Se hizo click!');
-    TIT.innerHTML = '¿Cómo se calcula?';
     const DATO = document.getElementById('peso').value
     //validamos que se cargue un dato:
-    if (DATO > 0 && DATO <=30 ){
-        TIT.style.display = 'none';
+    if (DATO > 0){
         ERROR.style.display = 'none'
-        let flujo = Math.round(calcFlujo(DATO)/24);
+        let flujo = calcFlujo(DATO);
         let mantenimiento = flujo*1.5;
         FLU.innerHTML = flujo + ' cc/hr';
         MAN.innerHTML = 'm+m/2 ' + mantenimiento + ' cc/hr';
-        MET.innerHTML = 'Método Holliday-Segar';
         FLU.style.display = 'block';
         MAN.style.display = 'block';
-        MET.style.display = 'block';
-        HOL.style.display = 'block';
-        VC.style.display = 'none';
-    } else if (DATO > 30) {
-        TIT.style.display = 'none'
-        ERROR.style.display = 'none'
-        let flujo = Math.round(calcFlujo(DATO)*1500); 
-        let mantenimiento = Math.round(flujo*20/15); 
-        FLU.innerHTML = 'Vol. diario min. ' + flujo + ' cc.';
-        MAN.innerHTML = 'Vol. diario max. ' + mantenimiento + ' cc.';
-        MET.innerHTML = 'Método de Volumen Corporal';
-        FLU.style.display = 'block';
-        MAN.style.display = 'block';
-        MET.style.display = 'block';
-        VC.style.display = 'block';
-        HOL.style.display = 'none';
-    } else if (DATO <= 0){
+    } else {
         ERROR.style.display = 'block';
         FLU.style.display = 'none';
         MAN.style.display = 'none';
-        VC.style.display = 'none';
-        HOL.style.display = 'none';
-        TIT.innerHTML = 'No se puede realizar el cálculo';
-        TIT.style.display = 'block';
-        MET.innerHTML = 'Por favor introduzca un peso superior a 0Kg.';
     }
 })
 
 
 
 function calcFlujo(peso) {
+    let hidra;
     let flujo;
     peso = Math.round(peso);
     if(peso <= 10) {
-        flujo = (100 * peso);
-    } else if (peso > 10 && peso <= 20){
-        flujo = 1000 + 50*(peso-10);
-    } else if(peso > 20 && peso <= 30){
-        flujo = 1500 + 20*(peso-20);
+        hidra = (100 * peso);
+    } else if (peso > 10 & peso <= 20){
+        hidra = 1000 + 50*(peso-10);
+    } else if(peso > 20 & peso <= 30){
+        hidra = 1500 + 20*(peso-20);
     } else if (peso>30){
-        flujo = ( (peso * 4) + 7) / (peso + 90);
+        hidra = ( (peso * 4) + 7) / (peso + 90);
     } 
-    console.log(flujo);
-    
+    if (peso<=30){
+        console.log("Se ha utilizado el método Holliday-Segar");
+        flujo = Math.round( hidra/24);
+        let mant  = Math.round( flujo*1.5);
+        console.log(`Volumen diario: ${hidra} cc.`);
+        console.log(`Mantenimiento: ${mant}  cc/h.`);
+    } else if (peso>30){
+        console.log("Se ha utilizado el método de Superficie Corporal");
+        let hid1 = Math.round( hidra * 1500 / 24);
+		let hid2 = Math.round( hidra * 2000 / 24);
+		console.log("Superficie Corporal 1 " + hid1);
+		console.log(`Superficie Corporal 2 ` + hid2);
+    }
 
     return flujo; 
 }
